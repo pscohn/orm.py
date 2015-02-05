9466620a54c250b37d1d92b38b1304e8ecb84212
Paul Cohn <pscohn@gmail.com>
2015-02-05 14:33:27 -0500

    Remove annotations for now

--- models.py
+++ models.py
@@ -2,19 +2,9 @@
 
 import orm
 
-''' You should first set up tables named "user" and "message" '''
 class User(orm.Model):
     username = orm.Field()
     password = orm.Field()
-
-    def get_all_received_messages(self):
-        ''' You could also pass in a limit and offset, or yield results
-            one at a time.
-        '''
-        return list(Message.all(user_to=self, order_by='-date_sent'))
-
-    def get_all_sent_messages(self):
-        return list(Message.all(user_from=user, order_by='-date_sent'))
 
 class Message(orm.Model):
     user_from = orm.ForeignKey(User) # field should be named "user_from_id"
