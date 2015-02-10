bc914ce294ad1cbbd47d24969e85745ae4780ecc
Paul Cohn <pscohn@gmail.com>
2015-02-10 11:11:33 -0500

    Fix module not exporting

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='orm.py',
-    version='0.1',
+    version='0.1.1',
     description='A minimal ORM for postgres',
     long_description=open('README.md').read(),
     url='https://github.com/pscohn/orm.py',
@@ -13,6 +13,7 @@ setup(
     license='MIT',
     keywords='orm object relational mapper postgres postgresql',
     install_requires=['psycopg2'],
+    py_modules=['orm'],
     
     classifiers=[
     'Development Status :: 3 - Alpha',
